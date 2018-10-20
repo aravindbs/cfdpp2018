2f01197d028ffb4fb2d78c364660b309f4d44092
Aravind B.S <bs.aravind.97@gmail.com>
2018-10-20 14:11:25 +0530

    removed JS Glue

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -4,23 +4,27 @@ from flask import Flask, Blueprint
 
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
-import yaml 
+import yaml
+import os 
+from dotenv import load_dotenv
 
+load_dotenv(dotenv_path='.env')
+'''
 def load_config ():
     with open ('../config.yml') as f:
         config = yaml.load(f)
         return config 
-
-config = load_config()
+'''
+# config = load_config()
 
 app = Flask(__name__)
-jsglue = JSGlue(app) 
-app.config['MONGO_URI'] = config['COSMOS_DB_URI']
+# jsglue = JSGlue(app) 
+app.config['MONGO_URI'] = os.getenv('COSMOS_DB_URI')
 app.secret_key = 'abcd'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = "users.login"
 mongo = PyMongo(app)
 
 from app.views.admin import admin
