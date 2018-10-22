84afefac7d22cda23721f6270a9c93ae91ab3328
Aravind B.S <bs.aravind.97@gmail.com>
2018-10-22 20:09:34 +0530

    refactoring

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -5,23 +5,18 @@ from flask import Flask, Blueprint
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
 import yaml
-import os 
+import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='.env')
-'''
-def load_config ():
-    with open ('../config.yml') as f:
-        config = yaml.load(f)
-        return config 
-'''
-# config = load_config()
+load_dotenv(dotenv_path='.env')  # initializes environment variables from the .env file 
+
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = config['COSMOS_DB_URI']
-app.secret_key = 'abcd'
 
+app.config['MONGO_URI'] = os.getenv('COSMOS_DB_URI')
+
+app.secret_key = 'abcd'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "users.login"
@@ -31,5 +26,6 @@ from app.views.admin import admin
 from app.views.users import users
 app.register_blueprint(users)
 app.register_blueprint(admin, url_prefix='/admin')
+
 
 from app.views import index
